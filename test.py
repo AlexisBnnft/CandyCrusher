41cbe91aab6a0ef7000c7c64da0626a6d5b61d5f
Alexis Bonnafont <bonnaf@stanford.edu>
2024-11-08 14:20:23 -0800

    Finishing basics tests

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ class TestBoard(unittest.TestCase):
         # |3 4 3 1 5 5|
         # |3 1 1 2 1 2|
         np.random.seed(0)   
-        board = Board(5,5)
+        board = Board(5,6)
         board.board = np.array([
                 [Candy(5, "normal"), Candy(5, "normal"), Candy(3, "normal"), Candy(1, "normal"), Candy(5, "normal"), Candy(5, "normal")],
                 [Candy(3, "normal"), Candy(6, "normal"), Candy(1, "normal"), Candy(3, "normal"), Candy(3, "normal"), Candy(5, "normal")],
@@ -27,19 +27,19 @@ class TestBoard(unittest.TestCase):
 
     def initialize_normal_board_2nd(self):
         # Initialize this board
-        # |5 5 3 6 5 5|
+        # |3 5 3 6 5 5|
         # |3 6 6 3 6 6|
-        # |1 1 3 6 1 6|
+        # |1 3 3 6 1 6|
         # |3 4 3 1 5 5|
-        # |3 1 1 2 1 2|
+        # |3 1 1 2 1 1|
         np.random.seed(0)   
-        board = Board(5,5)
+        board = Board(5,6)
         board.board = np.array([
-                [Candy(5, "normal"), Candy(5, "normal"), Candy(3, "normal"), Candy(6, "normal"), Candy(5, "normal"), Candy(5, "normal")],
+                [Candy(3, "normal"), Candy(5, "normal"), Candy(3, "normal"), Candy(6, "normal"), Candy(5, "normal"), Candy(5, "normal")],
                 [Candy(3, "normal"), Candy(6, "normal"), Candy(6, "normal"), Candy(3, "normal"), Candy(6, "normal"), Candy(6, "normal")],
-                [Candy(1, "normal"), Candy(1, "normal"), Candy(3, "normal"), Candy(6, "normal"), Candy(1, "normal"), Candy(6, "normal")],
+                [Candy(1, "normal"), Candy(3, "normal"), Candy(3, "normal"), Candy(6, "normal"), Candy(1, "normal"), Candy(6, "normal")],
                 [Candy(3, "normal"), Candy(4, "normal"), Candy(3, "normal"), Candy(1, "normal"), Candy(5, "normal"), Candy(5, "normal")],
-                [Candy(3, "normal"), Candy(1, "normal"), Candy(1, "normal"), Candy(2, "normal"), Candy(1, "normal"), Candy(2, "normal")]
+                [Candy(3, "normal"), Candy(1, "normal"), Candy(1, "normal"), Candy(2, "normal"), Candy(1, "normal"), Candy(1, "normal")]
             ])
         return board
 
@@ -129,23 +129,37 @@ class TestBoard(unittest.TestCase):
 
     def test_simple_sachet_creation_step_by_step_T_shape(self):
         board = self.initialize_normal_board_2nd()
-        print()
-        board.display_with_type()
-        print("move performed:")
-        board.display_move(((1,4),(1,3)))
         Action(board).swap(1,4,1,3,step_by_step=True)
-        print()
-        board.display_with_type()
         assert board.board[1,3].type == "sachet"
         board.make_it_fall()
         board.fill_random()
         board.update()
-        print()
-        board.display()
         assert board.board[2,3].type == "sachet"
 
+    def test_simple_sachet_creation_T_shape(self):
+        board = self.initialize_normal_board_2nd()
+        Action(board).swap(1,4,1,3)
+        assert board.board[2,3].type == "sachet"
+    
+    def test_simple_disco_horiz(self):
+        board = self.initialize_normal_board_2nd()
+        Action(board).swap(3,3,4,3)
+        assert board.board[4,3].type == "disco"
 
-        
+        # It should have created another disco
+        disco_count = 0
+        for i in range(board.board.shape[0]):
+            for j in range(board.board.shape[1]):
+                if board.board[i,j].type == "disco":
+                    disco_count += 1
+        assert disco_count == 2
 
+    def test_simple_disco_vert(self):
+        board = self.initialize_normal_board_2nd()
+        Action(board).swap(2,0,2,1)
+        assert board.board[4,0].type == "disco"
+
+
+    
 if __name__ == '__main__':
     unittest.main()
