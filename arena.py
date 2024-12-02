331937389a958397bcbdfaa71479c1473d931c2b
Alexis Bonnafont <bonnaf@stanford.edu>
2024-12-01 23:37:22 -0800

    adding arena

--- arena.py
+++ arena.py
@@ -3,10 +3,12 @@ from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor
 from board import Board
 from master import Master
+import time as time_module
 
-N = 200
-strategies = ["random", "mcts_base", "mcts_fixed_depth", "offline", "combined"]
+N = 1000
+strategies = ["random", 'greedy', "offline", "mcts_base", "mcts_fixed_depth", "combined"]
 results = pd.DataFrame(columns=strategies, index=range(N))
+time_df = pd.DataFrame(columns=strategies, index=range(N))
 
 
 def simulate_game(i):
@@ -15,23 +17,30 @@ def simulate_game(i):
     b.update()
     b.score = 0
     game_results = {}
+    time_results = {}
     for s in strategies:
         b_test = b.copy()
         m = Master(s, b_test)
+        start_time = time_module.time()
         score = m.run_simulation()
+        end_time = time_module.time()
         game_results[s] = score
-    return i, game_results
+        time_results[s] = end_time - start_time
+    return i, game_results, time_results
 
 
 def main():
     with ProcessPoolExecutor(max_workers=60) as executor:
         futures = [executor.submit(simulate_game, i) for i in range(N)]
         for future in tqdm(futures):
-            i, game_results = future.result()
+            i, game_results, time_results = future.result()
             for s, score in game_results.items():
                 results.loc[i, s] = score
+            for s, duration in time_results.items():
+                time_df.loc[i, s] = duration
 
     results.to_csv("results_arena.csv", index=False)
+    time_df.to_csv("time_arena.csv", index=False)
 
 
 if __name__ == "__main__":
--- master.py
+++ master.py
@@ -8,13 +8,15 @@ from board import Action
 
 strategy_params = {
     'random': {},
+    'greedy': {},
     'mcts_base': {
         'exploration_param': 5000,
         'N_rollout': 4,
         'n_simulation': 1500,
         'no_log': True,
         'write_log_file': False,
-        'model': None
+        'model': None,
+        'N_random': 3
     },
     'mcts_fixed_depth': {
         'exploration_param': 5000,
@@ -23,7 +25,8 @@ strategy_params = {
         'fixed_depth': 4,
         'no_log': True,
         'write_log_file': False,
-        'model': None
+        'model': None,
+        'N_random': 3
     },
     'offline': {
         'model_path': 'model.pth'
@@ -35,7 +38,8 @@ strategy_params = {
         'fixed_depth': 4,
         'no_log': True,
         'write_log_file': False,
-        'model': 'model.pth'
+        'model': 'model.pth',
+        'N_random': 3
     }
 }
 
@@ -64,6 +68,8 @@ class Master:
             return self.offline_strategy()
         elif self.strategy == 'combined':
             return self.combined_strategy(params)
+        elif self.strategy == 'greedy':
+            return self.greedy_strategy(params)
         else:
             raise ValueError(f"Unknown strategy: {self.strategy}")
         
@@ -106,8 +112,26 @@ class Master:
                 best_score = score
                 best_move = move
         return best_move
+
+    def greedy_strategy(self):
+        legal_moves = self.board.get_legal_moves()
+        if not legal_moves:
+            return None
+        best_move = None
+        best_score = float('-inf')
+        for move in legal_moves:
+            new_board = self.board.copy()
+            a=Action(new_board)
+            a.swap(*move[0], *move[1])
+            score = (new_board.score - self.board.score)
+            if score > best_score:
+                best_score = score
+                best_move = move
+        return best_move
     
     def combined_strategy(self, params):
+        params_excluding_last = {k: params[k] for k in list(params)[:-1]}
+        N_random = params['N_random']
         params['model'] = self.model
-        mcts = MCTS_CandyCrush_Complex(self.board, **params)
-        return mcts.best_move()
+        mcts = MCTS_CandyCrush_Complex(self.board, **params_excluding_last)
+        return mcts.best_move(N_random = N_random)
