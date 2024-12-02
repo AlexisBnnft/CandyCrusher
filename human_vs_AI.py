bba1f65cd169f30603652ba3ae8b7446275faad4
Alexis Bonnafont <bonnaf@stanford.edu>
2024-12-01 16:34:38 -0800

    reloading previous huma.csv

--- human_vs_AI.py
+++ human_vs_AI.py
@@ -9,17 +9,24 @@ import pandas as pd
 
 
 N_b = 200
-human_df = pd.DataFrame(columns=["score", 'board_init'], index = range(N_b))
+try:
+    human_df = pd.read_csv("human.csv")
+    human_df.drop("Unnamed: 0", axis=1, inplace=True)
+    print("Resuming previous session")
+except:
+    print("Initializing new session")
+    human_df = pd.DataFrame(columns=["score", 'board_init'], index = range(N_b))
 for i in range(10):
+    first_nan_index = human_df['score'].isnull().idxmax()
     b = Board(7,7)
     b.fill_random()
     b.update()
     b.score = 0
     v = Viz(b, Action(b), stop_at_10=True)
-    human_df.iloc[i]['board_init'] = b.state()
+    human_df.iloc[first_nan_index + i]['board_init'] = b.state()
     try:
         v.Visualize()
     except:
-        human_df.iloc[i]['score'] = b.score
+        human_df.iloc[first_nan_index + i]['score'] = b.score
 
-    human_df.to_csv("human1.csv")
+    human_df.to_csv("human.csv")
